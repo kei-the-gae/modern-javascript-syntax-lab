addc68131281841ba7e91107e3eff005fb729053
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-12 13:50:00 -0700

    Add exercise 2

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 const doubleNums = nums.map(num => { return num * 2 });
 // expected: [26, 174, 4, 178, 24, 8, 180, 126]
 // console.log(doubleNums);
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const [firstTopping, secondTopping] = pizzaToppings;
+// expected: firstTopping = 'Pineapple', secondTopping = 'Olives'
+// console.log(firstTopping);
+// console.log(secondTopping);
