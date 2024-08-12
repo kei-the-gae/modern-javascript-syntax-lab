fb3df50ae989834b452649cea2462dfe2a91dd77
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-12 14:07:58 -0700

    Add exercise 6

--- app.js
+++ app.js
@@ -61,3 +61,16 @@ myCar.model = 'q7';
 // expected: car = { make: 'Audi', model: 'q5' }, myCar = { make: 'Audi', model: 'q7' }
 // console.log('Original: ', car);
 // console.log('Clone: ', myCar);
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+const propertyName = 'age';
+const userProfile = {
+    [propertyName]: 27,
+};
+// expected: userProfile = { age: 27 }
+// console.log(userProfile);
